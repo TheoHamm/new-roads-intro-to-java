fe52c49d536508e3a1761c113dba11e55ac9f895
thamm <thamm@newroads.org>
2016-09-16 12:24:09 -0400

    Compute Grade app

---
+++ computegrade.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class ComputeGrade{
+
+  public static void main(String[] args){
+  
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Student's name: ");
+    
+    String name = input.next();
+    
+    System.out.println("Enter grade for test 1: ");
+  
+    
+    double grade1 = input.nexDouble();
+    
+    System.out.println("Enter grade for test 2: ");
+  
+    
+    double grade2 = input.nexDouble();
+    
+    System.out.println("Enter grade for test 3: ");
+  
+   
+    double grade3 = input.nexDouble();
+    
+    
+  
+    
+  
+  }
+  
+}
+
+ 
+

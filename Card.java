601412443f4934b7e76ad026c86fdf9a51bd7481
thamm <thamm@newroads.org>
2017-01-06 12:36:27 -0500

    card object

---
+++ Card.java
@@ -0,0 +1,21 @@
+public class Card{
+ 
+  
+  String rank; 
+  
+  String color;
+  
+  String suit;
+
+  public Card(){
+    rank = "A";
+    color = "black";
+    suit = "club";
+  }
+
+  public String toString(){
+    return rank + " " + color + " " + suit;
+  }
+  
+  
+}
